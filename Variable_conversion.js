b61a2fc364e5bf8ae67fd7f6ce1f1478fa379692
Avinash kushawaha <Mauryaavinash085@gmail.com>
2024-11-06 22:45:02 +0530

    first commit

---
+++ Variable_conversion.js
@@ -0,0 +1,29 @@
+// let score = true
+
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+
+// console.log(valueInNumber);
+
+// "33" => 33
+// "33abdc" => Nan
+// true => 1 ; false => 0
+
+let isLogggedIn = "hitesh"
+
+let booleanIsLogged = Boolean(isLogggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// " " => false
+// "hitesh" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log
---
+++ data_types.js
@@ -0,0 +1,31 @@
+// Primitive
+
+// 7 types : String, Number, Boolearn, null, underfined, Symbol, bigint
+
+
+// Reference (Non primitive)
+// Array, Object, Functions
+const score = 100
+const scoreValue = 100.3
+
+const isLogggedIn = false
+const outsideTemp = null
+let usersideEmail;
+ 
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id == anotherId);
+
+const heros = ["shaktiman", "naagraj0", "doga"];
+let myObj = {
+    name: "Javascript"
+    age: 22
+}
+
+
+const myFunction = function(){
+    console.log("Hello world");
+}
+
+console.log(typeof big)
