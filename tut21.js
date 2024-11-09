8c693ecde51879d8936c450a2b847e9a83a8c31c
Avinash kushawaha <Mauryaavinash085@gmail.com>
2024-11-09 14:07:25 +0530

    first commit

---
+++ tut21.js
@@ -0,0 +1,14 @@
+let quarter = 1;
+
+switch (quarter) {
+    case 1 : console.log("January, February, March");
+    break;
+    case 2 : console.log("April, May, Jun");
+    break;
+    case 3 : console.log("July, August, September");
+    break;
+    case 4 : console.log("October, November, December");   
+    break;
+    default:
+    console.log("Invalid quarter");
+}
---
+++ tut23.js
@@ -0,0 +1,13 @@
+if (a > b){
+    if(a > c) {
+        console.log(a, "is largest");
+    } else {
+        console.log(c, "is largest");
+    }
+} else {
+    if(b > c) {
+        console.log(b, "is largest");
+} else {
+    console.log(c, "is largest");
+}
+}
