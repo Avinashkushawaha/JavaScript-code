b3b3f3244a1656a24772de118da00f1fa9d5de8f
Avinash kushawaha <Mauryaavinash085@gmail.com>
2024-11-10 22:23:51 +0530

    first commit

---
+++ loop.js
@@ -0,0 +1,3 @@
+for (let i = 0; i < 5; i++) {
+    console.log(i);  // Prints numbers 0 to 4
+}
---
+++ tut25.js
@@ -0,0 +1,5 @@
+let result = 0 / 0;
+console.log(result);  // NaN
+
+let num = Number('hello');
+console.log(num);  // NaN
