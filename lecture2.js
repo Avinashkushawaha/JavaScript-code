6852a70ed98c10974943aa80c56813c8d4e7e6f3
Avinash kushawaha <Mauryaavinash085@gmail.com>
2024-11-12 11:25:32 +0530

    first commit

---
+++ lecture2.js
@@ -0,0 +1,14 @@
+ let a = [true,null,[2,"hy"], 10,"hello"];
+ console.log(a)
+// console.log(a[0]);
+// console.log(a[2][0]);
+
+a.push(100,2100);
+console.log(a)
+a.pop();
+console.log(a);
+a.unshift("first", 'qpider')
+console.log(a);
+a.shift();
+console.log(a);
+
