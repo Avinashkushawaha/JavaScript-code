ef3847174775b22b2ac12ed0acb4bf6ac132f810
Avinash kushawaha <Mauryaavinash085@gmail.com>
2024-11-12 11:54:29 +0530

    first commit

---
+++ lecture3.js
@@ -0,0 +1,13 @@
+let a = [true, null,[2, "hy"],10,"hello"];
+// console.log(a);
+
+let b = a.slice(1,4)
+console.log(b);
+
+// a.splice(1,2,"h1","h2","h3")
+// a.splice(1,0);
+// console.log(a);
+
+// console.log.(a[0]);
+// console.log(a[2][0]);
+
