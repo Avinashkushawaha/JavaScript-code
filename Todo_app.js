76b13239e69aa92e94fc622ff59beb8e1c8a7e5e
Avinash kushawaha <Mauryaavinash085@gmail.com>
2024-11-14 00:33:23 +0530

    update commit

--- Todo_app.js
+++ Todo_app.js
@@ -8,7 +8,21 @@ while(true) {
         break;
     }
     if(req == "list"){
-        
+        console.log("------------");
+        for(task of todo){
+            console.log(task);
+        }
+        console.log("--------------");
+    } else if(req == "add"){
+        let  task = prompt("please enter the task you want to add");
+        todo.push(task);
+        console.log("task added")
+    } else if(req == "delete"){
+        let idx = prompt("please enter the task index");
+        todo.splice(idx, 1);
     }
-}
+       req = prompt("please enter your request"); 
+
+    }
+
 
