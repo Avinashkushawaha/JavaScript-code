dcc41818ea74e63fd37ab9609fac8a641cde08a3
Avinash kushawaha <Mauryaavinash085@gmail.com>
2024-11-14 11:40:06 +0530

    first commit

---
+++ lecture5.js
@@ -0,0 +1,38 @@
+let str = "Qs psi dser";
+console.log(str.length);
+
+console.log(str[0]);
+console.log(str[4]);
+
+let str1 = str.toUpperCase();
+console.log(str1);
+
+let str2 = str1.toLowerCase();
+console.log(str2);
+
+let str3 = str.slice(0, 4);
+console.log(str3);
+
+let str4 = str.indexOf();
+console.log(str4);
+
+let str5 = str.lastIndexOf();
+console.log(str5);
+
+let str6 = str.charAt();
+console.log(str6);
+
+let str7 = str.split();
+console.log(str7);
+
+let str8 = str.replace();
+console.log(str8);
+
+let str9 = str.split();
+console.log(str7);
+
+
+
+
+
+
