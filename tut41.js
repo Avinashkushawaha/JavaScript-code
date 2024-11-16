859c3863fa388b5d7acd6100701b176ce1982eaa
Avinash kushawaha <Mauryaavinash085@gmail.com>
2024-11-17 01:19:53 +0530

    first commit

---
+++ tut41.js
@@ -0,0 +1,19 @@
+function oddOrEvenFactory(request) {
+    if(request == "odd") {
+      let add = function(n) {
+          console.log(!(n%2 == 0));
+}
+
+     return odd;
+}else if(request == "even") {
+let even = function(n) {
+    console.log(n%2 == 0);
+}
+  
+   return even; 
+    } else {
+        console.log("wrong request")
+    }
+}
+
+let request = "odd";
