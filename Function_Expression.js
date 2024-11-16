94a459d28d235d892f1e7b930f6a9607d0b09ebe
Avinash kushawaha <Mauryaavinash085@gmail.com>
2024-11-17 00:56:19 +0530

    first commit

---
+++ Function_Expression.js
@@ -0,0 +1,9 @@
+let name = "shradha";
+
+let sum = function(a, b) {
+    return a+b;
+}
+
+let hello = function() {
+    console.log("hello");
+}
---
+++ tut37.js
@@ -0,0 +1,8 @@
+function outerFunc() {
+    let x = 5;
+    let y = 6;
+    function innerFunc() {
+        console.log(x);
+    }
+    innerFunc();
+}
