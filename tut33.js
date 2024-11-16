ef4908bc5b380d449b7fcd95a3417ffaace06cc9
Avinash kushawaha <Mauryaavinash085@gmail.com>
2024-11-16 20:41:01 +0530

    first commit

---
+++ tut33.js
@@ -0,0 +1,5 @@
+function printTable(n) {
+    for(let i=n; i<=n*10; i+=n) {
+        console.log(i);
+    }
+}
---
+++ tut34.js
@@ -0,0 +1,8 @@
+function isAdult(age) {
+    if(age >= 18) {
+        return "adult";
+    } else {
+        return "not adult";
+    }
+    console.log("bye bye");
+}
