1304f833afe8f10e5b95b43fd4730697f89137b4
Avinash kushawaha <Mauryaavinash085@gmail.com>
2024-11-19 21:21:05 +0530

    first commit

---
+++ tut46.js
@@ -0,0 +1,13 @@
+// arr.forEach(some function definition or name);
+
+let arr = [1, 2, 3, 4, 5];
+
+function print(el) {
+    console.log(el);
+}
+
+arr.forEach(print);
+
+arr.forEach(function(el) {
+    console.log(el);
+});
