3be230bb3e0e07350dcc6715b0bb91149473cf0b
Avinash kushawaha <Mauryaavinash085@gmail.com>
2024-11-20 22:26:48 +0530

    first commit

--- Todo_app.js
+++ Todo_app.js
@@ -1,28 +1,28 @@
-let todo = [];
+// let todo = [];
 
-let req = prompt("please enter your request");
+// let req = ("please enter your request");
 
-while(true) {
-    if(req == "quit") {
-        console.log("quitting app");
-        break;
-    }
-    if(req == "list"){
-        console.log("------------");
-        for(task of todo){
-            console.log(task);
-        }
-        console.log("--------------");
-    } else if(req == "add"){
-        let  task = prompt("please enter the task you want to add");
-        todo.push(task);
-        console.log("task added")
-    } else if(req == "delete"){
-        let idx = prompt("please enter the task index");
-        todo.splice(idx, 1);
-    }
-       req = prompt("please enter your request"); 
+// while(true) {
+//     if(req == "quit") {
+//         console.log("quitting app");
+//         break;
+//     }
+//     if(req == "list"){
+//         console.log("------------");
+//         for(task of todo){
+//             console.log(task);
+//         }
+//         console.log("--------------");
+//     } else if(req == "add"){
+//         let  task = ("please enter the task you want to add");
+//         todo.push(task);
+//         console.log("task added")
+//     } else if(req == "delete"){
+//         let idx = ("please enter the task index");
+//         todo.splice(idx, 1);
+//     }
+//        req = ("please enter your request"); 
 
-    }
+//     }
 
 
