9ffe8f7108ed8871bd3a97d788904db168f3c11f
Avinash kushawaha <Mauryaavinash085@gmail.com>
2024-11-22 00:03:50 +0530

    first coomit

---
+++ tut54.js
@@ -0,0 +1,5 @@
+//  Create a new array using the map function whose each element is equal to the original element plus 5
+
+let numbers = [2, 4, 6, 8, -2, -4];
+
+console.log (numbers.map((numbers) => number + 5));
---
+++ tut55.js
@@ -0,0 +1,5 @@
+// Create a new array whose elements are in uppercase of words present in the original array
+
+let strings = ["adam", "bob", "catlyn", "donald", "eve"];
+
+console.log(strings.map((string) => string.toUpperCase()));
