4720931c66d078fda11f69e1f854bb5514c2ff0f
Avinash kushawaha <Mauryaavinash085@gmail.com>
2025-02-06 22:22:57 +0530

    update commit

--- primitive_data_type_in.js
+++ primitive_data_type_in.js
@@ -1,8 +1,17 @@
-let a = null;
-let b = 345;
-let c = true;
-let d = bigInt("567")
-let e = "avi"
-let f = Symbol("I am a nice symbol")
-console.log(a, b, c, d, e, f, g)
-console.log(typeof d)
+// let a = null;
+// let b = 345;
+// let c = true;
+// let d = bigInt("567")
+// let e = "avi"
+// let f = Symbol("I am a nice symbol")
+// console.log(a, b, c, d, e, f, g)
+// console.log(typeof d)
+
+const item = {
+    "shubh": true,
+    "Rohan": false,
+    "titu": 67,
+    "love": undefined
+}
+
+console.log(item['shubh'])
