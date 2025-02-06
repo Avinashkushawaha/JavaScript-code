f6dbc60a7ad90d82155d1a6e8309c624ca0ebb4c
Avinash kushawaha <Mauryaavinash085@gmail.com>
2025-02-06 22:44:57 +0530

    initial commit

--- practice_set1.js
+++ practice_set1.js
@@ -1,6 +1,17 @@
-let a = "4"
-let b = "5"
+// let a = "4"
+// let b = "5"
 
-console.log(a + b)
+// console.log(a + b)
+// console.log(typeof (a+b))
 
-console.log(typeof (a+b))
+// a1['friend'] = 'shubham'
+// a1['name'] = 'lovish'
+// console.log(a1)
+
+const dict = {
+    appreciate: "recognise the full worth of .",
+    ataraxia: "a state of freedom from emotional disturbance and anxiety",
+    yakka: "work, especially hard work."
+}
+
+console.log(dict['yakka'])
