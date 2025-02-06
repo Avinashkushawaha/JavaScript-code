4919a8954f4b08bc6ef49fb12c025ccd35280eb7
Avinash kushawaha <Mauryaavinash085@gmail.com>
2025-02-06 22:07:01 +0530

    update commit

--- const_let_and_var_in_javascript.js
+++ const_let_and_var_in_javascript.js
@@ -1,1 +1,17 @@
-consi
+console.log("Javascript tutorial var, let, const")
+// var a = 45;
+// var a = 'p'
+let b = 'harry'
+const author = 'avi'
+
+author = 5
+b = 4
+const harry = 0 ;
+let c = null
+let d = undefined
+{
+    let b = 'this'
+    console.log(b)
+
+}
+console.log(b)
