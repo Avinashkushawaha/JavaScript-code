2ab944d76fecec978fb785d700ad73e64b3ca33f
Avinash kushawaha <Mauryaavinash085@gmail.com>
2025-02-08 01:19:40 +0530

    initial commit

---
+++ practice_set2.js
@@ -0,0 +1,21 @@
+// if(age>10 && age <20){
+    //     console.log("your age lies between 10 and 20")
+    // }
+    // else{
+        //     console.log("Your age doesnt lies between 10 and 20")
+        // }
+        
+let age = prompt(" what is the your age")
+switch (age) {
+    case 12:
+        console.log("your age is 12")
+    case 13:
+        console.log("your age is 13")
+    case 14:
+        console.log("your age is 14")
+    case 15:
+        console.log("your age is 15")
+    default:
+        console.log("your age is not special")
+
+}
