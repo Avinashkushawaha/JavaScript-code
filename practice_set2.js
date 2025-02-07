3aca50f8f30b2caa32a85ff2149117bee5ebb07a
Avinash kushawaha <Mauryaavinash085@gmail.com>
2025-02-08 01:23:25 +0530

    update commit

--- practice_set2.js
+++ practice_set2.js
@@ -10,11 +10,14 @@ switch (age) {
     case 12:
         console.log("your age is 12")
     case 13:
+    
         console.log("your age is 13")
     case 14:
         console.log("your age is 14")
     case 15:
+
         console.log("your age is 15")
+        break
     default:
         console.log("your age is not special")
 
