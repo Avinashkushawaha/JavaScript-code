cb3cadbdca6052b32d1a7846fb9c70ed290e2d28
Avinash kushawaha <Mauryaavinash085@gmail.com>
2025-02-08 01:05:47 +0530

    update commit

--- conditinal_statement.js
+++ conditinal_statement.js
@@ -1,6 +1,22 @@
-let a = promp("hey whats your age")
-a = Number.parseInt(a)
-console.log(typeof a) 
-// if(a>0){
-//     alert()
+// let a = promp("hey whats your age")
+// a = Number.parseInt(a)
+// console.log(typeof a) 
+//  if(a>0){
+//      alert()
 // }
+
+if (age > 0){
+    console.log("A valid age")
+
+}
+else if (age > 10 , age < 15){
+    console.log("but you are a kid");
+
+}
+else if (age > 18){
+    console.log("not a kid");
+}
+else {
+    console.log("invalid Age")
+   
+}
