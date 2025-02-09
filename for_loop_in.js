5f880eb7e96bcf8f192fc52b59c9330a4a0ec82b
Avinash kushawaha <Mauryaavinash085@gmail.com>
2025-02-09 21:44:50 +0530

    update commit

--- for_loop_in.js
+++ for_loop_in.js
@@ -2,3 +2,9 @@ for (let i = 0; i < 10; i++) {
     console.log(i)
 }
 
+let sum = 0
+let n = prompt("Enter a the value of n")
+for (let i = 0; i < n; i++) {
+    sum += i
+}
+console.log("Sum of first " + n + " natural numbers is " + sum)
