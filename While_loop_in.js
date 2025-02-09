1ff1a3946863954e8a8e466548831b1f6bf427c1
Avinash kushawaha <Mauryaavinash085@gmail.com>
2025-02-09 22:08:56 +0530

    Add do-while loop

---
+++ While_loop_in.js
@@ -0,0 +1,13 @@
+let a = promt("Enter the value of a")
+a = Number.parseInt(a)
+
+// while (a <= 10) {
+//     console.log(a)
+//     a++
+// }
+
+do {
+    console.log(a)
+    a++
+} while (a <= 10)
+
