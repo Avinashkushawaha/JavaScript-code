b03c8db0b4b65dc1f078a312e02f9f9745ed180a
Avinash kushawaha <Mauryaavinash085@gmail.com>
2025-02-09 21:53:37 +0530

    uodate commit

--- for_loop_in.js
+++ for_loop_in.js
@@ -1,10 +1,20 @@
-for (let i = 0; i < 10; i++) {
-    console.log(i)
+// for (let i = 0; i < 10; i++) {
+//     console.log(i)
+// }
+
+// let sum = 0
+// let n = prompt("Enter a the value of n")
+// for (let i = 0; i < n; i++) {
+//     sum += i
+// }
+// console.log("Sum of first " + n + " natural numbers is " + sum)
+
+let obj = {
+    name: "John",
+    age: 20,
+    city: "New York"
 }
 
-let sum = 0
-let n = prompt("Enter a the value of n")
-for (let i = 0; i < n; i++) {
-    sum += i
+for (let a in obj) {
+    console.log(a)
 }
-console.log("Sum of first " + n + " natural numbers is " + sum)
