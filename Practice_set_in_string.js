ec3821b220ce2dcebbebc39a045e23e70cab6e29
Avinash kushawaha <Mauryaavinash085@gmail.com>
2025-02-10 21:53:03 +0530

    update commit

---
+++ Practice_set_in_string.js
@@ -0,0 +1,20 @@
+// // console.log("har\"".length)
+
+
+// const sentence = 'The qucik brown fox jumps over the lazy dog';
+// const word = 'fox';
+
+// console.log(sentence.includes(word))
+// console.log(`The word "${word}" ${sentence.includes(word) ? 'is' : 'is not'} in the sentence`);
+//  console.log(sentence.startsWith("The")) 
+//  console.log(sentence.endsWith("dog"))
+
+let str = "Please give Rs 1000";
+let amount = Number.parseInt(str.slice(15));
+
+// console.log(amount)
+console.log(typeof amount)
+
+
+
+
