bcbf2c9018982aaf091080ff0aac0495d34da28c
Avinash kushawaha <Mauryaavinash085@gmail.com>
2025-02-10 17:58:09 +0530

    update commit

---
+++ Function_in.javascript.js
@@ -0,0 +1,12 @@
+function onePlusAvg(x, y) {
+    return 1 + (x + y) / 2
+}
+
+let a = 1
+let b = 2
+let c = 3
+
+console.log("One plus average of a and b is ", onePlusAvg(a, b))
+console.log("One plus average of a and b is ", onePlusAvg(a, b))
+console.log("One plus average of a and b is ", onePlusAvg(a, b))
+
