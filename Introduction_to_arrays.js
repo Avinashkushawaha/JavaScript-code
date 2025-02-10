a79c5cf12ce17e3c2d47b0f95aee3c0865ef7ee0
Avinash kushawaha <Mauryaavinash085@gmail.com>
2025-02-10 22:13:17 +0530

    Add array

---
+++ Introduction_to_arrays.js
@@ -0,0 +1,24 @@
+let marks = [91, 32, 39, 46, 52, false, "not present"]
+
+for (let m of marks) {
+    console.log(m)
+}
+
+// console.log(marks[0])
+// console.log(marks[1])
+// console.log(marks[2])
+// console.log(marks[3])
+// console.log(marks[4])
+
+// console.log(marks[9])
+
+// console.log(marks[0] = 99)
+// console.log(marks[1] = 100)
+// console.log(marks[2] = 101)
+// console.log(marks[3] = 102)
+// console.log(marks[4] = 103)
+// console.log(typeof marks)
+
+
+
+
