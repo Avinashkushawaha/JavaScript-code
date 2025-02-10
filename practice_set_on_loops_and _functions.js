c273dda25ac4d2e6c01cd66f5d57aea9d7fac36c
Avinash kushawaha <Mauryaavinash085@gmail.com>
2025-02-10 19:22:45 +0530

    update commit

---
+++ practice_set_on_loops_and _functions.js
@@ -0,0 +1,13 @@
+let marks = {
+    harry: 98,
+    shubham: 95,
+    rohan: 75,
+    aakash: 70,
+}
+
+for (let i = 0; i < Object.keys(marks).length; i++) {
+    console.log("The marks of " + Object.keys(marks)[i] + " are " + marks[Object.keys(marks)[i]])
+}
+
+
+
