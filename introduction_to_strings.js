0b09cc617ed51a9b52e970e887dff95577181b7c
Avinash kushawaha <Mauryaavinash085@gmail.com>
2025-02-10 19:45:07 +0530

    uppdate commit

--- introduction_to_strings.js
+++ introduction_to_strings.js
@@ -1,5 +1,13 @@
-let name = "harry";
-console.log(name.length);
+// let name = "harry";
+// console.log(name.length);
 
-let friend = "Rohan";
-console.log(friend)
+// let friend = "Rohan";
+// console.log(friend)
+
+// Template literals
+let boy1 = "Rohan";
+let boy2 = "Shubham";
+
+let sentence = `${boy1} is a friend of ${boy2}`;
+console.log(sentence);
+
