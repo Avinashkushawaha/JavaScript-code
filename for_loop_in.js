63469e67d09963bfa4972486e20230862f4e55f5
Avinash kushawaha <Mauryaavinash085@gmail.com>
2025-02-10 21:59:52 +0530

    update commit

--- for_loop_in.js
+++ for_loop_in.js
@@ -9,12 +9,49 @@
 // }
 // console.log("Sum of first " + n + " natural numbers is " + sum)
 
-let obj = {
-    name: "John",
-    age: 20,
-    city: "New York"
+// let obj = {
+//     name: "John",
+//     age: 20,
+//     city: "New York"
+// }
+
+// for (let a in obj) {
+//     console.log(a)
+// }
+
+// 1.for loop
+// for (let i = 0; i < 10; i++) {
+//     console.log(i)
+// }
+
+// 2.while loop
+// let i = 0;
+// while (i < 10) {
+//     console.log(i)
+//     i++
+// }
+
+// 3.do while loop
+// let i = 0;
+// do {
+//     console.log(i)
+//     i++
+// } while (i < 10)
+
+// 4.for in loop
+// let obj = {
+//     name: "John",
+//     age: 20,
+//     city: "New York"
+// }
+// for (let a in obj) {
+//     console.log(a, obj[a])
+// }
+
+// 5.for of loop
+let arr = [1, 2, 3, 4, 5]
+for (let value of arr) {
+    console.log(value);
 }
 
-for (let a in obj) {
-    console.log(a)
-}
+
