f35a342c83dd078f04678d2a0d4896651de39cb7
Avinash kushawaha <Mauryaavinash085@gmail.com>
2025-02-10 19:13:18 +0530

    update commit

--- Function_in.javascript.js
+++ Function_in.javascript.js
@@ -10,3 +10,4 @@ console.log("One plus average of a and b is ", onePlusAvg(a, b))
 console.log("One plus average of a and b is ", onePlusAvg(a, b))
 console.log("One plus average of a and b is ", onePlusAvg(a, b))
 
+
