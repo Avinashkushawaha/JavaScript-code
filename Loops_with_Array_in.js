b5c835af96211916a01e079e11ce8bb6d46cbf2c
Avinash kushawaha <Mauryaavinash085@gmail.com>
2025-02-11 18:52:25 +0530

    Demonstrate array iteration techniques using for...in, forEach, and commented examples

--- Loops_with_Array_in.js
+++ Loops_with_Array_in.js
@@ -1,1 +1,30 @@
-let num 
+let num = [3, 5, 1, 2, 4]
+
+// for (let i = 0; i < num.length; i++) {
+//     console.log(num[i])
+// }
+
+//  num.forEach((element) => {
+//     console.log(element * element)
+// })
+
+
+// let name = "harry"
+// let arr = Array.from(name)
+// console.log(arr)
+
+// for (let i of num) {
+//     console.log(i)
+// }
+
+for (let i in num) {
+    console.log(num[i])
+}
+
+
+
+
+
+
+
+
