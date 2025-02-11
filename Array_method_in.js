b3138f1f91e78556b7c97a03af85ef1a539967ce
Avinash kushawaha <Mauryaavinash085@gmail.com>
2025-02-11 18:11:41 +0530

    Implement array methods: toString and push

---
+++ Array_method_in.js
@@ -0,0 +1,16 @@
+let num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+let b = num.toString()
+console.log(b)
+
+// let c = num.join("_")
+// console.log(c)
+
+// let d = num.pop()
+// console.log(d)
+
+let e = num.push(11)
+console.log(e)
+
+
+
