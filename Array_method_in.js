839dff3a611877d6b7d20a23c052daa2c9b48721
Avinash kushawaha <Mauryaavinash085@gmail.com>
2025-02-11 18:37:11 +0530

    Implement array method: splice and modify initial array

--- Array_method_in.js
+++ Array_method_in.js
@@ -1,4 +1,4 @@
-let num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+let num = [100, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 let b = num.toString()
 console.log(b)
@@ -15,8 +15,32 @@ console.log(b)
 // let f = num.shift()
 // console.log( f, num)
 
-let g = num.unshift(11)
-console.log(g, num)
+// let g = num.unshift(11)
+// console.log(g, num)
+
+
+
+// delete num[0]
+// console.log(num.length)
+
+// let compare = (a, b) => {
+//     return a - b
+// }
+// // num.sort(compare)
+// // console.log(num)
+
+// num.reverse()
+// console.log(num)
+
+num.splice(1, 2, "banana", "orange")
+console.log(num)
+
+
+
+
+
+
+
 
 
 
