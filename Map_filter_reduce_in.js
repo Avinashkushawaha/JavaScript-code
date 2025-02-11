f05e515b8db26038b73c42c1771d8bea2ce14252
Avinash kushawaha <Mauryaavinash085@gmail.com>
2025-02-11 19:07:57 +0530

    Demonstrate array filtering with reduce, map

---
+++ Map_filter_reduce_in.js
@@ -0,0 +1,19 @@
+let arr = [45, 23, 21, 0, 3, 5]
+
+// let a = arr.map((value) => {
+//     console.log(value)
+// })
+
+
+let a = arr.filter((value) => {
+    return value > 10
+})
+console.log(a)
+
+
+// let a = arr.reduce((value1, value2) => {
+//     return value1 + value2
+// })
+// console.log(a)
+
+
