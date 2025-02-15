cf631f5d08a7bb95cc4a9e49cd683015e9d565f3
Avinash kushawaha <Mauryaavinash085@gmail.com>
2025-02-15 22:49:01 +0530

    update commit

---
+++ variables.js
@@ -0,0 +1,13 @@
+const accountId = 144553;
+let accountEmail = "hdfc@gmail.com";
+var accountPassword = "21212121";
+accountCity = "Jaipur";
+
+
+accountEmail = "hc@hc.com";
+accountPassword = "21212121212121";
+accountCity = "Jaipur";
+
+console.log(accountPassword);
+
+console.table([accountId, accountEmail, accountPassword, accountCity]);
