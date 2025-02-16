203902b7558f710ad0834b0b4c5d4568cff9f977
Avinash kushawaha <Mauryaavinash085@gmail.com>
2025-02-16 15:21:52 +0530

    update commit

--- Array_in_.js
+++ Array_in_.js
@@ -1,13 +1,15 @@
-const myArr = [0, 1, 2, 3, 4, 5];
-console.log(myArr);
+// const myArr = [0, 1, 2, 3, 4, 5];
+// console.log(myArr);
 
-const myArr1 = myArr.slice(1, 3);
-console.log(myArr1);
+// const myArr1 = myArr.slice(1, 3);
+// console.log(myArr1);
 
-const myArr2 = myArr.splice(1, 3);
-console.log(myArr2);
-console.log(myArr);
+// const myArr2 = myArr.splice(1, 3);
+// console.log(myArr2);
+// console.log(myArr);
 
+const Array = [1, 2, 3, 4, 5];
 
+console.log(Array.from({name: "John"}));
 
 
