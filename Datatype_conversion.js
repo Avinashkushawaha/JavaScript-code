fe0e0b4ab09e71e411514302fbe719a4bce62231
Avinash kushawaha <Mauryaavinash085@gmail.com>
2025-02-16 11:58:23 +0530

    update commit

--- Datatype_conversion.js
+++ Datatype_conversion.js
@@ -8,4 +8,21 @@
 
 
 
+// console.log(null > 0);
+// console.log(null >= 0);
+// console.log(null == 0);
+// console.log(null === 0);
+
+// console.log(undefined == 0);
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+const num = null;
+
+console.log(typeof num);
+
+
+
+
+
+
 
