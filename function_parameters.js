9b622cbf5641e19cf4796b6bac8db3ac2711f46e
Avinash kushawaha <Mauryaavinash085@gmail.com>
2025-02-16 16:16:15 +0530

    update commit

---
+++ function_parameters.js
@@ -0,0 +1,28 @@
+// function addTwoNumbers(num1, num2) {
+//     console.log(num1 + num2);
+
+// }
+
+// addTwoNumbers(5, 10);
+
+// function calculateCartPrice(val1, val2, ...num1) {
+    
+//     return num1;
+// }
+
+// console.log(calculateCartPrice(200, 400, 500, 700));
+
+
+const user = {
+    username: "hitesh",
+    price: 199
+}
+
+function handleObject(anyobject) {
+    console.log(`username is ${anyobject.username} and price is ${anyobject.price}`);
+}
+
+handleObject(user);
+
+
+
