0b370e1fad39ee914bd592bb1f16137ee5ee328d
Avinash kushawaha <Mauryaavinash085@gmail.com>
2025-02-17 18:58:51 +0530

    update commit

---
+++ Global_and_local_scope.js
@@ -0,0 +1,32 @@
+// let a = 10;
+// const b = 20;
+// // var c = 30;
+
+
+// {
+//     let a = 1;
+//     const b = 2;
+//     console.log("inner", a);
+//     console.log(b);
+// }
+// console.log(a);
+// console.log(b);
+
+
+addOne(5);
+function addOne(num){
+    return num + 1;
+}
+
+
+const addTwo = function(num){
+    return num + 2;
+}
+
+addTwo(5);
+
+
+
+
+
+
