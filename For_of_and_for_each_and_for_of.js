82194f7ebd497866b63b2714f8020b08fb771227
Avinash kushawaha <Mauryaavinash085@gmail.com>
2025-02-18 02:16:39 +0530

    update commit

--- For_of_and_for_each_and_for_of.js
+++ For_of_and_for_each_and_for_of.js
@@ -7,8 +7,49 @@ for (const num of arr) {
 const greetings = "Hello World"
 
 for (const greet of greetings) {
-    console.log(`Each character is ${greet}`);
+    // console.log(`Each character is ${greet}`);
 }
+
+// maps
+
+const map = new Map()
+map.set('IN', "India")
+map.set('USA', "United States of America")
+map.set('FR', "France")
+
+
+for (const [key, value] of map) {
+    // console.log(key, ':-', value);
+}
+
+// const myObject = {
+//     game1: 'NFS',
+//     game2: 'Spiderman'
+// }
+
+// for (const [key, value] of myObject) {
+    // console.log(key, ':-', value);
+
+
+
+
+const myObject = {
+    js: 'javascript',
+    cpp: 'C++',
+    rb: "ruby",
+    swift: "swift by apple"
+}
+for (const key in myObject) {
+    // console.log(`${key} shortcut is for ${myObject[key]}`);
+}   
+
+
+const programming = ["js", "rb", "py", "java", "cpp"]
+
+programming.forEach(function (item) {
+    console.log(item);
+})
+
 
 
 
