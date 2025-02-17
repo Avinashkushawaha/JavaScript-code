33f602af0b738d893be00e072305f33486cb3d39
Avinash kushawaha <Mauryaavinash085@gmail.com>
2025-02-17 19:33:23 +0530

    initial commit

--- IIFF.js
+++ IIFF.js
@@ -1,7 +1,18 @@
 // IIFE (Immediately Invoked Function Expression)
-
+// Named IIFE
 (function chai(){
     console.log("DB CONNECTED");
 })();
+
+// Unnamed IIFE
+((named) =>{
+    console.log(`DB CONNECTED ${named}`);
+})("hitesh");
+
+
+
+
+
+
 
 
