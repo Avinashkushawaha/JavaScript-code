d13017b76e309849bb994851bed1db41afa59c83
Avinash kushawaha <Mauryaavinash085@gmail.com>
2025-02-17 23:15:55 +0530

    update commit

--- Iterations_in.js
+++ Iterations_in.js
@@ -16,10 +16,11 @@
 
 
 
-for (let index = 0; index < array.length; index++) {
-    const element = array[index];
-    console.log(element);
+for (let index = 1; index <= 10; index++) {
+  if (index == 5){
+      console.log(`detected 5`)
+    continue;
 }
+console.log(`value of i is ${index}`);
 
-
-
+}
