301fdccae3767e362eb1b9a0cd6c9e825fe0656e
Avinash kushawaha <Mauryaavinash085@gmail.com>
2025-02-18 01:55:20 +0530

    update commit

---
+++ While_loop.js
@@ -0,0 +1,29 @@
+// let index = 0;
+// while (index <= 10) {
+//     console.log(`value of index is ${index}`);
+//     index = index + 2;
+// }
+
+
+let myArray = ["flash", "batman", "superman"];
+
+// for (let index = 0; index < myArray.length; index++) {
+//     const element = myArray[index];
+//     console.log(element);
+// }
+
+// let arr = 0
+// while (arr < myArray.length) {
+//     console.log(myArray[arr]);
+//     arr = arr + 1;
+// }
+
+let score = 1
+do {
+    console.log(`Score is ${score}`);
+    score = score + 1;
+} while (score <= 10);
+
+
+
+
