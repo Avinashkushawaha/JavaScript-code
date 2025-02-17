cbfe7b66f21d1f618c0a4e62a5e66d4b878211d7
Avinash kushawaha <Mauryaavinash085@gmail.com>
2025-02-17 22:26:15 +0530

    update commit

--- Control_flow.js
+++ Control_flow.js
@@ -1,7 +1,31 @@
-const userLoggedIn = true;
+// const userLoggedIn = true;
 
-if(userLoggedIn){
+// if(userLoggedIn){
+//     console.log("Allow to change text");
+// }
+
+// const balance = 1000;
+
+// if (balance > 500) {
+//     console.log("test");
+
+// } else if (balance < 500) {
+//     console.log("not enough balance");
+// } else if (balance == 0) {
+//     console.log("balance is 0");
+// } else {
+//     console.log("balance is negative");
+// }
+
+const userLoggedIn = true;
+const debitCard = true;
+
+if (userLoggedIn && debitCard) {
     console.log("Allow to change text");
 }
+
+
+
+
 
 
