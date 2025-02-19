698333c83ff225e88205fe65a5ca9b61d0b72bde
Avinash kushawaha <Mauryaavinash085@gmail.com>
2025-02-20 00:37:58 +0530

    update commit

--- staticprop.js
+++ staticprop.js
@@ -1,3 +1,25 @@
 class User {
-    
+    constructor(username){
+        this.username = username
+    }
+
+    logMe(){
+        console.log(`Username: ${this.username}`);
+    }
+    createId(){
+        return `123`
+    }
 }
+
+const Avi = new User("Avi");
+console.log(Avi.createId())
+
+class Teacher extends user {
+    constructor(username, email){
+        super(username)
+        this.email = email
+    }
+}
+
+const iphone = new Teacher("iphone", "i@phone.com")
+console.log(iphone.logMe);
