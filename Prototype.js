f132b35fe34f82888074060e67ea7d588a7519bd
Avinash kushawaha <Mauryaavinash085@gmail.com>
2025-02-19 23:45:32 +0530

    update commit

--- Prototype.js
+++ Prototype.js
@@ -1,1 +1,23 @@
-let myName
+let myName = "Avi"
+letmychannel = "chai"
+
+// console.log(myName.trim().length);
+
+let myHeros = ["thor", "spiderman"]
+
+let heroPower = {
+    thor: "hammer",
+    spiderman: "sling",
+
+
+    getSpiderPower: function(){
+        console.log(`Spidy power is ${this.spiderman}`)
+    }
+}
+
+Object.prototype.Avi = function(){
+    console.log(`Avi is present in all objects`);
+}
+
+// heroPower.Avi()
+myHeros.Avi()
