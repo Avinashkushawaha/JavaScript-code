a891e510ba991afdb884905b8d519f062c526781
Avinash kushawaha <Mauryaavinash085@gmail.com>
2025-02-19 23:11:53 +0530

    update commit

--- Oops_in.js
+++ Oops_in.js
@@ -1,13 +1,25 @@
-const user = {
-    username: "hitesh",
-    loginCount: 8,
-    signedIn: true,
+// const user = {
+//     username: "hitesh",
+//     loginCount: 8,
+//     signedIn: true,
 
-    getUserDetails: function(){
-       console.log(" Got user details from database")
-       console.log(`Username: ${this.username}`);
-    }
+//     getUserDetails: function(){
+//        console.log(" Got user details from database")
+//        console.log(`Username: ${this.username}`);
+//     }
+// }
+
+// console.log(user.username);
+// console.log(user.getUserDetails());
+
+function User(username, loginCount, isLoggedIn){
+    this.username = username;
+    this.loginCount = loginCount;
+    this.isloggedIn = isLoggedIn
+
+    return this
 }
 
-console.log(user.username);
-console.log(user.getUserDetails());
+const userOne = User("Avi", 12, true)
+const userTwo = User("ChaiaurCode", 11, false)
+console.log(userOne);
