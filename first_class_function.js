77f0f57d2872593b75152b2be6cc1bd99d6fa47b
Avinash kushawaha <Mauryaavinash085@gmail.com>
2025-02-23 23:16:32 +0530

    update commit

---
+++ first_class_function.js
@@ -0,0 +1,16 @@
+// Function Statement
+function a() {
+  console.log('Hi');
+}
+a();
+
+// Fucntion Expression
+var b = function () {
+  console.log('Hello');
+}
+b();
+
+// Anonymous Function
+var c = function () {
+  console.log('Bye');
+}
