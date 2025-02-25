df29c2ed36a45211e73405fb85a96f8cde3a0a5c
Avinash kushawaha <Mauryaavinash085@gmail.com>
2025-02-25 19:43:19 +0530

    update commit

--- map_filter_reduce.js
+++ map_filter_reduce.js
@@ -1,4 +1,4 @@
-const arr = [5, 1, 3, 2, 6];
+// const arr = [5, 1, 3, 2, 6];
 
 
 // const output = arr.map(function (element) {
@@ -15,15 +15,33 @@ const arr = [5, 1, 3, 2, 6];
 // const output = arr.filter(isOdd);
 // console.log(output);
 
-function findSum(arr) {_
-    let sum = 0;
-    for (let i = 0; i < arr.length; i++) {
-        sum += arr[i];
-    }
-    return sum;
+// function findSum(arr) {_
+//     let sum = 0;
+//     for (let i = 0; i < arr.length; i++) {
+//         sum += arr[i];
+//     }
+//     return sum;
 
-}
-console.log(findSum(arr));
-const outout = arr.reduce(function (acc, curr) {
-    return acc + curr;
-});
+// }
+// console.log(findSum(arr));
+// const outout = arr.reduce(function (acc, curr) {
+//     return acc + curr;
+// });
+
+
+const user =[
+{firstname: "harshit", lastname: "singh", age: 24},
+{firstname: "Avi", lastname: "maurya", age: 45},
+{firstname: "deep", lastname: "yug", age: 50},
+{firstname: "prem", lastname: "asthana", age: 24},
+];
+
+const output = user.reduce(function (acc, curr) {
+    if (acc[curr.age]) {
+        acc[curr.age] = ++acc[curr.age];
+    } else {
+        acc[curr.age] = 1;
+    }
+    return acc;
+}, {});
+console.log(output);
