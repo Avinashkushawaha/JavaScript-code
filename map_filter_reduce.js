63940068abbdac5b11cb5c6a84af440ce7e1152b
Avinash kushawaha <Mauryaavinash085@gmail.com>
2025-02-25 19:23:56 +0530

    update commit

--- map_filter_reduce.js
+++ map_filter_reduce.js
@@ -8,9 +8,22 @@ const arr = [5, 1, 3, 2, 6];
 
 // filter odd value
 
-function isOdd(x) {
-    return x % 2;
-}
+// function isOdd(x) {
+//     return x % 2;
+// }
 
-const output = arr.filter(isOdd);
-console.log(output);
+// const output = arr.filter(isOdd);
+// console.log(output);
+
+function findSum(arr) {_
+    let sum = 0;
+    for (let i = 0; i < arr.length; i++) {
+        sum += arr[i];
+    }
+    return sum;
+
+}
+console.log(findSum(arr));
+const outout = arr.reduce(function (acc, curr) {
+    return acc + curr;
+});
