054ab43ec1295c85cc5423906a3c0455d3d0ca80
Avinash kushawaha <Mauryaavinash085@gmail.com>
2025-02-27 22:18:26 +0530

    update commit

--- Promises.js
+++ Promises.js
@@ -1,8 +1,26 @@
-const cart = ["apple", "orange", "banana"];
+// const cart = ["apple", "orange", "banana"];
 
-createOrder(cart, function(orderId) {
-  proceedTopayment(orderId);
-});
+// createOrder(cart, function(orderId) {
+//   proceedTopayment(orderId);
+// });
 
 
 // const promise = createOrder(cart);
+
+
+
+
+const  cart = ["apple", "orange", "banana"];
+
+createOrder(cart, function(orderId) {
+  proceedTopayment(orderId, function (paymenInfo){
+    showOrderSummary(paymentInfo, function (){
+        updateWalletBalance();
+    });
+  });
+});
+
+// createOrder(cart)
+// .then(orderId) =>  proceedTopayment(orderId))
+// .then(paymentInfo) => showOrderSummary(paymentInfo))
+// .then(()=> updateWalletBalance(paymentInfo));
