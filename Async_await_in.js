91626051d6a250f650d2a52d8734fc00359013e1
Avinash kushawaha <Mauryaavinash085@gmail.com>
2025-02-28 22:16:02 +0530

    update commit

--- Async_await_in.js
+++ Async_await_in.js
@@ -9,16 +9,36 @@
 // dataPromise.then((res) => console.log(res))
 
 
-const p = new Promise((resolve, reject) => {
-    setTimeout(() => {
-     resolve("Promise Resolved Value !!");
-    }, 1000)
+// const p = new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//      resolve("Promise Resolved Value !!");
+//     }, 1000)
+// });
+
+// // await can only be used inside an asyn function
+// async function handlePromise(){
+//     const val = await p;
+//     console.log(val);
+// }
+
+// handlePromise();
+
+
+const p2 = new Promise((resolve, reject) =>{
+    setTimeout(() =>{
+        resolve("promise Resolved Value!!");
+    }, 1000);
 });
 
-// await can only be used inside an asyn function
 async function handlePromise(){
-    const val = await p;
-    console.log(val);
-}
+    console.log("Hello world!!");
 
+    const val = await p1;
+    console.log("Hello javaScript");
+    console.log(val);
+
+    const val2 = await p2;
+    console.log("Namaste javaScript2");
+    console.log(val2)
+}
 handlePromise();
