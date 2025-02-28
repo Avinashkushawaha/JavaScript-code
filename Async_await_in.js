91d81eef4ece44461b35389a73ea16ce83143186
Avinash kushawaha <Mauryaavinash085@gmail.com>
2025-02-28 21:41:24 +0530

    update commit

--- Async_await_in.js
+++ Async_await_in.js
@@ -1,6 +1,21 @@
-async function getData() {
-    return "hello world"
+// const p = new promise((resolve, reject) => {
+//     resolve("Promise Resolved Value");
+// })
+// async function getData() {
+//     return "hello world"
+// }
+
+// const dataPromise = getData();
+// dataPromise.then((res) => console.log(res))
+
+
+const p = new Promise((resolve, reject) => {
+    resolve("Promise Resolved Value !!");
+});
+
+async function handlePromise(){
+    const val = await p;
+    console.log(val);
 }
 
-const data = getData();
-console.log(data);
+handlePromise();
