51c24eab0e92dd43ab542bcb6d273e4219c2a640
Avinash kushawaha <Mauryaavinash085@gmail.com>
2025-03-18 00:00:19 +0530

    update commit

--- debounce_function_in.js
+++ debounce_function_in.js
@@ -2,6 +2,9 @@ function debounce(func, delay) {
     let timeout;
     return function(...args) {
         clearTimeout(timeout);
-        timeout = setTimeout(() =>
-    }
+        timeout = setTimeout(() =>func.apply(this, args), delay);
+    };
 }
+
+const debouncedFunction = debounce(() => console.log("Executed"), 500);
+window.addEventListener("resize", debouncedFunction);
