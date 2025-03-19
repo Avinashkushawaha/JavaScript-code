cb898295cab4a29bc0fbaa09f778549e42440b32
Avinash kushawaha <Mauryaavinash085@gmail.com>
2025-03-19 22:18:45 +0530

    update commit

--- This_keyword_in.js
+++ This_keyword_in.js
@@ -1,4 +1,12 @@
 const obj = {
     name : "Alice",
-    greet : funct
-}
+    greet : function() {
+        console.log(this.name);
+    },
+    greetArrow: () => {
+        console.log(this.name);
+    }
+};
+
+obj.greet();
+obj.greetArrow();
