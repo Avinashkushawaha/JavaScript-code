c0b84406d6b4246439b467828d3165089599d37b
Avinash kushawaha <Mauryaavinash085@gmail.com>
2025-03-21 20:50:18 +0530

    update commit

--- asynchronous_pattern.js
+++ asynchronous_pattern.js
@@ -6,3 +6,9 @@ getData((data) => {
     });
 });
 
+async function handlerData() {
+    const data = await getData();
+    const processData = await processData(data);
+    await saveData(processData);
+    console.log('Done');
+}
