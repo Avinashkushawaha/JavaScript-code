e0bd292d061d6807d944ad52a5f3b7705dd8689f
Avinash kushawaha <Mauryaavinash085@gmail.com>
2025-03-27 23:43:45 +0530

    update commit

---
+++ virtual_dom_diffing_algorithm.js
@@ -0,0 +1,45 @@
+function diff(oldNode, newNode) {
+    if (!oldNode) {
+        return { type: 'CREATE', newNode };
+    }
+
+    if (!newNode) {
+        return { type: 'REMOVE' };
+    }
+
+    if (Changed(oldNode, newNode)) {
+        return { type: 'REPLACE', node:newNode };
+    }
+
+    if (newNode.type) {
+        const patches = {
+            type: 'UPDATE',
+            children: [],
+            attrs: diffAttributes(oldNode.props, newNode.props)
+        };
+
+        const oldChildren = oldNode.children || [];
+        const newChildren = newNode.children || [];
+        const maxLen = Math.max(oldChildren.length, newChildren.length);
+
+        for (let i = 0; i < maxLen; i++) {
+            patches.children.push(diff(oldChildren[i], newChildren[i]));
+        }
+
+        return patches;
+    }
+
+    return { type: 'SKIP' };
+}
+
+function Changed(node1, node2) {
+    return (
+        typeof node1 !== typeof node2 ||
+        (typeof node1 === 'string' && node1 !== node2) ||
+        node1.type !== node2.type
+    );
+}
+
+function diffAttributes(oldProps = {}, newProps = {}) {
+    
+}
