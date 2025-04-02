b38452937bafa6d7bd4bff3b3f8f4b2f171cdb21
Avinash kushawaha <Mauryaavinash085@gmail.com>
2025-04-02 23:55:13 +0530

    update commit

--- Reactice_data_store.js
+++ Reactice_data_store.js
@@ -16,5 +16,14 @@ class ReactiveStore {
         for (let i = 0; i < keys.length - 1; i++) {
             current = current[keys[i]];
         }
+
+        current[keys[keys.length - 1]] = value;
+        this.notify(path, value);
+    }
+
+    notify(path, value) {
+        for (const subscriber of this.subscribers) {
+            subscriber(path, value);
+        }
     }
 }
