268b2453f7d53af4c98f731be3c97cbd028d6edd
Avinash kushawaha <Mauryaavinash085@gmail.com>
2025-04-06 23:31:12 +0530

    update commit

---
+++ Dependency_injection_container.js
@@ -0,0 +1,49 @@
+class DIContainer {
+    constructor(){
+        this.registry = new Map();
+        this.instances = new Map();
+    }
+
+    register(name, factory, isSingleton = false) {
+        this.register.set(name, { factory, isSingleton });
+    }
+
+    resolve(name) {
+        if (!this.registry.has(name)) {
+            throw new Error(`Service ${name} not registerd`);
+        }
+
+        const { factory, isSingleton } = this.registry.get(name);
+
+        if (isSingleton) {
+            if (!this.instances.has(name)) {
+                this.instances.set(name, factory(this));
+            }
+            return this.instances.get(name);
+        }
+
+        return factory(this);
+    }
+
+}
+
+const container = new DIContainer();
+
+container.register('logger', () => {
+    return {
+        log: console.log,
+        error: console.error
+    };
+
+}, true);
+
+container.register('apiClient', (container) => {
+    const logger = container.resolve('logger');
+    return {
+        fetchData: () => {
+            logger.log('Fetching data...');
+            return Promise.resolve({ data: 'sample' });
+        }
+    };
+});
+
