a276fa4813c828dc26887180747b34bb1096be9a
Avinash kushawaha <Mauryaavinash085@gmail.com>
2025-04-07 23:31:25 +0530

    update commit

--- Reactive_computed_properties.js
+++ Reactive_computed_properties.js
@@ -15,4 +15,44 @@ class ReactiveStore {
         });
         this._state[key] = initiaValue;
     }
+
+    computed(key, fn, deps) {
+        this._computed.set(key, fn);
+        this._dependencies.set(key, deps);
+
+        Object.defineProperty(this, key, {
+            get: () => {
+                const shouldRecompute = deps.some(dep =>
+                    this._state[`${key}_lastDeps`]?.[dep] !== this._state[dep]
+                );
+
+                if (shouldRecompute || !(`${key}_value` in this._state)) {
+                    this._state[`${key}_value`] = fn.call(this);
+                    this._state[`${key}_lastDeps`] = deps.reduce((acc, dep) => {
+                        acc[dep] = this._state[dep];
+                        return acc;
+                    }, {});
+
+                }
+
+                return this._state[`${key}_value`];
+            }
+        })
+    }
+    _triggerDependents(changeKey) {
+        for (const [computedKey, deps] of this._dependencies) {
+            if (deps.includes(changeKey)) {
+                delete this._state[`${computedKey}_value`];
+            }
+        }
+    }
 }
+
+const store = new ReactiveStore();
+store.defineProperty('price', 10);
+store.defineProperty('quantity', 2);
+store.computed('total', () => store.price * store.quantity, ['price', 'quantity']);
+
+console.log(store.total);
+store.price = 15;
+console.log(store.total);
