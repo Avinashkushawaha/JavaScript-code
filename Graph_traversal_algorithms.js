911b204f56af7febcbad7dd261cc49ef0d572826
Avinash kushawaha <Mauryaavinash085@gmail.com>
2025-04-09 23:45:30 +0530

    update commit

--- Graph_traversal_algorithms.js
+++ Graph_traversal_algorithms.js
@@ -32,5 +32,56 @@ class Graph {
         }
         return result;
     }
-    
+    dfs(start){
+        const visited = new Set();
+        const result = [];
+ 
+        const traverse = (node) =>{
+            visited.add(node);
+            result.push(node);
+            for (const neighbor of this.adjacencyList.get(node)){
+                if (!visited.has(neighbor)){
+                    traverse(neighbor);
+                }
+            }
+        };
+        traverse(start);
+        return result;
+    }
+    dfsIterative(start) {
+        const visited = new Set();
+        const stack = [start];
+        const result = [];
+
+        while (stack.length) {
+            const node = stack.pop();
+            if (!visited.has(node)){
+                visited.add(node);
+                result.push(node);
+
+                const neighbors = this.adjacencyList.get(node);
+
+                for (let i = neighbors.length - 1; i >= 0; i --){
+                    if (!visited.has(neighbors[i])) {
+                        stack.push(neighbors[i]);
+                    }
+                }
+            }
+        }
+        return result;
+    }
 }
+
+const graph = new Graph();
+graph.addNode('A');
+graph.addNode('B');
+graph.addNode('C');
+graph.addEdge('D');
+graph.addEdge('A', 'B');
+graph.addEdge('A', 'C');
+graph.addEdge('B', 'D')
+graph.addEdge('C', 'D');
+
+console.log('BFS:', graph.bfs('A'));
+console.log('DFS:', graph.dfs('A'));
+console.log('DFS Iterative:', graph.dfsIterative('A'));
