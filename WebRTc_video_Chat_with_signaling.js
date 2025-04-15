0536b9fd57513c81b69c640955eba71cf4e963d2
Avinash kushawaha <Mauryaavinash085@gmail.com>
2025-04-15 22:20:42 +0530

    update commit

--- WebRTc_video_Chat_with_signaling.js
+++ WebRTc_video_Chat_with_signaling.js
@@ -66,7 +66,60 @@ class Videochat {
                     { ursl: 'stun:stun.l.google.com:19302'}
                 ]
             });
+
+            this.localStream.getAudioTracks().forEach(track => {
+                this.peerConnection.addTrack(track, this.localStream);
+            });
+
+            this.peerConnection.ontrack = (event) => {
+                this.remoteVideo.srcObject = event.streams[0];
+            };
+
+            this.peerConnection.onicecandidate = (event) => {
+                if (event.candidate) {
+                    this.socket.emit('ice-candidate', {
+                        candidate: event.candidate,
+                        roomId: this.roomId
+                    });
+                }
+            };
+
+            await this.peerConnection.setRemoteDescription(offer);
+            const answer  = await this.peerConnection.createAnswer();
+            await this.peerConnection.setLocalDescription(answer);
+            this.socket.emit('answer', {
+                answer,
+                roomId: this.roomId
+            });
         }
+    }
+
+    async handleAnswer({ answer }) {
+        if (!this.peerConnection) return;
+        await this.peerConnection.setRemoteDescription(answer);
+    }
+
+    async handleNewICECandidate({ candidate }) {
+        if (!this.peerConnection) return;
+        await this.peerConnection.addIceCandidate(candidate);
+    } catch (err) {
+        console.log("Error adding ICE candidate: ", err);
+    }
+
+    hangUp() {
+        if (this.peerConnection) {
+            this.peerConnection.close();
+            this.peerConnection = null;
+        }
+        if (this.localStream) {
+            this.localStream.getTracks().forEach(track => track.stop());
+            this.peerConnection = null;
+        }
+         this.remoteVideo.srcObject = null;
+         this.localVideo.srcObject = null;
+         this.socket.emit('leave-room', this.roomId);
+        
+    
     }
 }
 
