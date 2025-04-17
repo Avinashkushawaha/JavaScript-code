fcf1a26e9740b907ea1da185aee4a45b38052dfa
Avinash kushawaha <Mauryaavinash085@gmail.com>
2025-04-17 23:39:39 +0530

    update commit

--- Web_Audio_API_Synthesizer_with_Effects.js
+++ Web_Audio_API_Synthesizer_with_Effects.js
@@ -28,5 +28,48 @@ class Synthesizer {
 
         const feedback = this.audioCtx.createGain();
         feedback.gain.value = 0.5;
+
+        const wetGain = this.audioCtx.createGain();
+        wetGain.gain.value = 0.3;
+
+        delay.connect(feedback);
+        feedback.connect(delay);
+        delay.connect(wetGain);
+
+        return wetGain;
+    }
+
+    createReverb() {
+        const convolver = this.audioCtx.createConvolver();
+
+        const wetGain = this.audioCtx.createGain();
+        wetGain.gain.value = 0.2;
+
+        const sampleRate = this.audioCtx.sampleRate;
+        const length = sampleRate * 2;
+        const impulse = this.audioCtx.createBuffer(2, length, sampleRate);
+        const left = impulse.getChannelData(0);
+        const right = impulse.getChannelData(1);
+
+        for (let i = 0; i < length; i++) {
+            left[i] = (Math.random() * 2 - 1) * Math.pow(1 - i / length, 2);
+            right[i] = (Math.random() * 2 - 1) * Math.pow(1 - i / length, 2);
+
+        }
+        convolver.buffer = impulse;
+        convolver.connect(wetGain);
+
+        return wetGain;
+    }
+
+    noteOn(note, velocity = 1.0) {
+        if (this.oscillators.has(note)) return;
+
+        const osc = this.audioCtx.createOscillator();
+        const gain = this.audioCtx.createGain();
+
+        osc.type = this.waveform;
+        osc.frequency.value = this.noteToFrequency(note);
+        gain.gain.value = 0;
     }
 }
