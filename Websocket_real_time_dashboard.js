f19f20917864ed32f8fa3662f629b674f442da06
Avinash kushawaha <Mauryaavinash085@gmail.com>
2025-04-18 23:59:40 +0530

    update commit

--- Websocket_real_time_dashboard.js
+++ Websocket_real_time_dashboard.js
@@ -9,4 +9,52 @@ class RealTimeDashboard {
         this.initSocket();
         this.initUI();
     }
+
+    initSocket() {
+        this.socket.onopen = () =>{
+            console.log('Websocket connected');
+            this.socket.send(JSON.stringify({ type: 'subscribe'}));
+        };
+
+        this.socket.onmessage = (event) => {
+            const data = JSON.parse(event.data);
+            this.procesUpdate(data);
+
+        };
+
+        this.socket.onclose = () =>{
+            console.log('Websocket disconnected');
+            setTimeout(() => this.reconnect(), 5000);
+        };
+    }
+
+    reconnect() {
+        this.socket = new WebSocket(this.socketUrl);
+        this.initSocket();
+    }
+
+    initUI() {
+        this.container.innerHTML = `<div class="dashboard-header>
+          <h2>Real-Time Dashboard</h2>
+          <div class="connection-status">Connected </div>
+          </div>
+          <div class="charts-container"></div> `;
+    }
+
+    procesUpdate(update) {
+        if (!this.dataBuffers.has(update.metric)){
+            this.createChart(update.metric);
+            this.dataBuffers.set(update.metric, []);
+        }
+
+        const buffer = this.dataBuffers.get(update.metric);
+        buffer.push({
+            timestamp: new Date(update.timestamp),
+            value: update.value
+        });
+
+        if (buffer.length> this.maxDataPoints){
+            
+        }
+    }
 }
