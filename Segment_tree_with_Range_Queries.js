0c5a27a5e98e060d4a7cc77e9551f05f4d583abf
Avinash kushawaha <Mauryaavinash085@gmail.com>
2025-04-20 22:39:47 +0530

    update commit

--- Segment_tree_with_Range_Queries.js
+++ Segment_tree_with_Range_Queries.js
@@ -1,1 +1,27 @@
-c
+class SegmentTree {
+    constructor(arr) {
+        this.n = arr.length;
+        this.size = 1;
+        while (this.size < this.n) this.size <<= 1;
+        this.tree = new Array(2 * this.size).fill(0);
+
+        for (let i = 0; i < this.n; i++) {
+            this.tree[this.size + i] = arr[i];
+
+        }
+        for (let i = this.size - 1; i > 0; i--) {
+            this.tree[i] = this.tree[2 * i] + this.tree[2 * i + 1];
+        }
+}
+
+update(index, value) {
+       let pos = this.size + index;
+       this.tree[pos] = value;
+       pos >>= 1;
+       
+       while (pos >= 1) {
+        this.tree[pos] = this.tree[2 * pos] + this.tree[2 * pos + 1];
+        pos >>= 1;
+       }
+    }
+}
