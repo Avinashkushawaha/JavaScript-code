e78ac58748aff52758e20c5e1a537b2484ae6419
Avinash kushawaha <Mauryaavinash085@gmail.com>
2025-04-21 21:32:06 +0530

    update commit

---
+++ Skip_list_implementation.js
@@ -0,0 +1,62 @@
+class SkipListNode {
+    constructor(value, level) {
+        this.value = value;
+        this.forward = new Array(level + 1).fill(null);
+    }
+}
+
+class SkipList {
+    constructor(maxLevel = 16, p = 0.5) {
+        this.maxLevel = maxLevel; 
+        this.p = p;
+        this.level = 0;
+        this.header = new SkipListNode(null, maxLevel);
+    }
+    randomLevel() {
+        let level = 0;
+        while (Math.random() < this.p && level < this.maxLevel) {
+            level++;
+        }
+        return level;
+    }
+   search(target) {
+        let current = this.header;
+        for (let i = this.level; i >= 0; i--) {
+            while (current.forward[i] && current.forward[i].value < target) {
+                current = current.forward[i];
+            }
+        }
+        current = current.forward[0];
+        return current && current.value === target ? current : null;
+    }
+   
+    insert(value) {
+        const update = new Array(this.maxLevel + 1).fill(null);
+        let current = this.header;
+        for (let i = this.level; i >= 0; i--) {
+            while (current.forward[i] && current.forward[i].value < value) {
+                current = current.forward[i];
+            }
+            update[i] = current;
+        }
+        current = current.forward[0];
+        if (!current || current.value !== value) {
+            const newLevel = this.randomLevel();
+            if (newLevel > this.level) {
+                for (let i = this.level + 1; i <= newLevel; i++) {
+                    update[i] = this.header;
+                }
+                this.level = newLevel;
+            }
+            const newNode = new SkipListNode(value, newLevel);
+            for (let i = 0; i <= newLevel; i++) {
+                newNode.forward[i] = update[i].forward[i];
+                update[i].forward[i] = newNode;
+            }
+
+            return true
+        }
+        return false; 
+    }
+
+    
