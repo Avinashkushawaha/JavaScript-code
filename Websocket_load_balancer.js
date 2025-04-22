397602236508a786fb4a7ae6a310e80e806dc4f2
Avinash kushawaha <Mauryaavinash085@gmail.com>
2025-04-22 23:04:31 +0530

    update commit

--- Websocket_load_balancer.js
+++ Websocket_load_balancer.js
@@ -31,7 +31,64 @@ class WebSocketLoadBalancer {
         this.currentIndex = (this.currentIndex + 1) % this.sockets.length;
         return this.sockets[this.currentIndex];
     }
+    _handleOpen() {
+        const hasOpenConnection = this.sockets.some(socket => socket.readyState === WebSocket.OPEN);
+
+        if (hasOpenConnection && !this.isConnected) {
+            this.isConnected = true;
+            this.connectionCallbacks.forEach(callback => callback(true));
+            this._processQueue();
+        }
+    }
+    _handleMessage(event) {
+        this.messageCallbacks.forEach(callback => callback(event.data));
+    }
+    _handleError(event) {
+        this.errorCallbacks.forEach(callback => callback(event));
+    }
+    _handleClose() {
+        const allClosed = this.sockets.every(
+            socket => socket.readyState === WebSocket.CLOSED 
+        );
+        if (allClosed && this.isConnected) {
+            this.isConnected = false;
+           
+            setTimeout(() => this._connect(), 5000);
+    }
+}
+_closeAll() {
+    this.sockets.forEach(socket => {
+        if (socket.readyState !== WebSocket.CLOSED) {
+            socket.close();
+        }
+    });
+}
+_processQueue() {
+    while (this.messagesQueue.length > 0 && this.isConnected) {
+        const {message, resolve, reject} = this.messagesQueue.shift();
+        this._send(message).then(resolve).catch(reject);
+    }
+}
+_send(message) {
+    return new Promise((resolve, reject) => {
+        if(!this.isConnected) {
+            this.messagesQueue.push({message, resolve, reject});
+            return;
+        }
+
+        try {
+            const socket = this._getNextSocket();
+            if (socket.readyState === WebSocket.OPEN) {
+            socket.send(message);
+            resolve();
+            } else {
+                throw new Error('Socket is not open');
+            }
+        } catch (error) {
+            reject(error);
+
+        }
+    });
 }
 
-   
-    
+}
