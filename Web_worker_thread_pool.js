e80f453ec9607c468f1536b4de2ced4143337c5f
Avinash kushawaha <Mauryaavinash085@gmail.com>
2025-04-25 21:59:50 +0530

    update commit

--- Web_worker_thread_pool.js
+++ Web_worker_thread_pool.js
@@ -1,1 +1,45 @@
-c
+class WorkerThreadPool {
+    constructor(poolSize, workerScript) {
+        this.poolSize = poolSize;
+        this.workerScript = workerScript;
+        this.workers = [];
+        this.taskQueue = [];
+        this.workersStatus = [];
+
+        for (let i = 0; i < poolSize; i++) {
+            const worker = new Worker(workerScript);
+            worker.onmessage = this._handleWorkerResponse.bind(this, i);
+            this.workers.push(worker);
+            this.workersStatus.push(false); // false means idle
+        }
+        }
+
+        execute(taskData) {
+            return new Promise((resolve, reject) => {
+                this.taskQueue.push({
+                    taskData,
+                    resolve,
+                    reject,
+                    reject
+                });
+                this._processNextTask();
+            });
+        }
+
+            _processNextTask() {
+                if (this.taskQueue.length === 0) return;
+
+                const  availableWorkerIndex = this.workersStatus.indexOf(false);    
+            if (availableWorkerIndex === -1) return; 
+
+            const task = this.taskQueue.shift();
+            this.workersStatus[availableWorkerIndex] = true;
+
+            this.workers[availableWorkerIndex].postMessage(task.taskData);
+
+            task.workerIndex = availableWorkerIndex; 
+    
+            }
+            
+        }
+    
