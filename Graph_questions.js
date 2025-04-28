fa7fc5bdfdc493458b17137dbe936659e6ece1e2
Avinash kushawaha <Mauryaavinash085@gmail.com>
2025-04-28 22:32:30 +0530

    update commit

--- Graph_questions.js
+++ Graph_questions.js
@@ -25,4 +25,41 @@ class Graph {
         }
         delete this.adjacencyList[vertex];
     }
+    depthFirstRecursive(start) {
+        const result = [];
+        const visited = {};
+        const adjacencyList = this.adjacencyList;
+
+        function dfs(vertex) {
+            if (!vertex) return null;
+            visited[vertex] = true;
+            result.push(vertex);
+            adjacencyList[vertex].forEach(neighbor => {
+                if (!visited[neighbor]) {
+                    return dfs(neighbor);
+                }
+            });
+        }(start);
+        return result;
+    }
+    
+    breadthFirst(start) {
+        const queue = [start];
+        const result = [];
+        const visited = {};
+        let currentVertex;
+
+        visited[start] = true;
+        while (queue.length) {
+            currentVertex = queue.shift();
+            result.push(currentVertex);
+            this.adjacencyList[currentVertex].forEach(neighbor => {
+                if (!visited[neighbor]) {
+                    visited[neighbor] = true;
+                    queue.push(neighbor);
+                }
+            });
+        }
+        return result;
+    }
 }
