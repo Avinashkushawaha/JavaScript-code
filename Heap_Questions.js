823fa5f32833194540029cf56a766f16654b9e19
Avinash kushawaha <Mauryaavinash085@gmail.com>
2025-04-29 21:34:36 +0530

    update commit

--- Heap_Questions.js
+++ Heap_Questions.js
@@ -6,4 +6,21 @@ class MinHeap {
     getParentIndex(index) {
         return Math.floor((index - 1) / 2);
     }
+
+    getLeftChildIndex(index) {
+        return 2  * index + 1;
+    }
+
+    getRightChildIndex(index) {
+        return 2 * index + 2;
+    }
+
+    swap(index1, index2) {
+        [this.heap[index1], this.heap[index2]] = [this.heap[index2], this.heap[index1]];
+    }
+
+    insert(value) {
+        this.heap.push(value);
+        this.heapifyUp();
+    }
 }
