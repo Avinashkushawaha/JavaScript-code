6761ef4d58e66a71d4a498f5508a427bcedc33d9
Avinash kushawaha <Mauryaavinash085@gmail.com>
2025-04-29 21:45:11 +0530

    update commit

--- Heap_Questions.js
+++ Heap_Questions.js
@@ -31,6 +31,33 @@ class MinHeap {
          index = this.getParentIndex(index);
         }
     }
+   extractMin() {
+    if (this.heap.length === 0) return null;
+    if (this.heap.length === 1) return this.heap.pop();
 
-    
-}
+    const min = this.heap[0];
+    this.heap[0] = this.heap.pop();
+    this.heapifDown();
+    return min;
+   }
+
+   heapifDown() {
+    let index = 0;
+    while (this.getLeftChildIndex(index) < this.heap.length) {
+        let smallerChildIndex = this.getLeftChildIndex(index);
+        if (this.getRightChildIndex(index) < this.heap.length &&
+     this.heap[this.getLeftChildIndex(index)] < this.heap[smallerChildIndex]) {
+        smallerChildIndex = this.getRightChildIndex(index);
+     }
+
+     if (this.heap[index] < this.heap[smallerChildIndex]) {
+        break;
+     } else {
+        this.swap(index, smallerChildIndex);
+     }
+     index = smallerChildIndex;
+     }
+    }
+   }
+
+
