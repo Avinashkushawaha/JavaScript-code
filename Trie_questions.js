283603a1d26bd951db885b09b2e1bd0d6ec2f3bf
Avinash kushawaha <Mauryaavinash085@gmail.com>
2025-04-30 22:35:05 +0530

    update commit

--- Trie_questions.js
+++ Trie_questions.js
@@ -4,9 +4,53 @@ class TrieNode {
         this.isEndOfWord = false;
     }
 }
-
 class Trie {
     constructor() {
         this.root = new TrieNode();
     }
+    insert(word) {
+        let node = this.root;
+        for (const char of word) {
+            if (!node.children[char]) {
+                node.children[char] = new TrieNode();
+            }
+            node = node.children[char];
+        }
+        node.isEndOfWord = true;
+    }
+
+    search(word) {
+        let node = this.root;
+        for (const char of word) {
+            if (!node.children[char]) {
+                return false;
+            }
+            node = node.children[char];
+        }
+        return node.isEndOfWord;
+        
+    }
+    startWith(prefix) {
+        let node = this.root;
+        for (const char of prefix) {
+            if(!node.children[char]){
+                return false;
+            }
+            node = node.children[char];
+        }
+        return true;
+    }
+
+    autoComplete(prefix) {
+        let node = this.root;
+        for (const char of prefix) {
+            if (!node.children[char]) {
+                return [];
+            }
+            node = node.children[char];
+        }
+        return this._getAllWordsFromNode(node, prefix);
+    }
+
+    _getAllWordsFromNode
 }
