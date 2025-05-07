9dbc6f11c3c225a5e3d5f0618e945fdf00aa68a1
Avinash kushawaha <Mauryaavinash085@gmail.com>
2025-05-07 22:23:02 +0530

    update commit

---
+++ Check_balanced_parantheses.js
@@ -0,0 +1,12 @@
+function isBalanced(str) {
+    const stack = [];
+    const pairs = { '(' : ')' , '[' : '] ' , '{' : '}' };
+    for (const char of str) {
+        if (pairs[char]) stack.push(char);
+        else if (char === ')' || char === ']' || char === '}') {
+            if (pairs[stack.pop()] !== char) return false;
+        }
+    }
+    return stack.length === 0;
+}
+console.log(isBalanced("({[]})"))
